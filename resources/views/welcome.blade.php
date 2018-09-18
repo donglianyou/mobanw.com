499e0732f0f14e05115b8e6e475d359f75e18dc3
donglianyou <158624818@qq.com>
2018-09-18 09:24:04 +0800

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<<<<<<< HEAD
         <title>模板小筑</title>
-=======
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
->>>>>>> 970cbe13b1c2e363c95774048e98781ff5200d0f
         <!-- Styles -->
         <style>
             html, body {
